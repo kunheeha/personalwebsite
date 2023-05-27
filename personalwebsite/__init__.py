84b1f23fd19bfe5e66e6cb6c31772155cdca3ae4
Kunhee Ha <kunheeha@gmail.com>
2023-05-27 13:46:50 +0100

    don't need Bcrypt yet

--- personalwebsite/__init__.py
+++ personalwebsite/__init__.py
@@ -1,13 +1,11 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 
 
 db = SQLAlchemy()
-bcrypt = Bcrypt()
 login_manager = LoginManager()
 mail = Mail()
 
@@ -17,7 +15,6 @@ def create_app_from_config():
     app = Flask(__name__)
 
     db.init_app(app)
-    bcrypt.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
 
