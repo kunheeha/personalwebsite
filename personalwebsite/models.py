4da3d6f9ab6bed1fdba214def90d67c2c27a3d6f
Kunhee Ha <kunheeha@gmail.com>
2023-05-31 17:20:54 +0100

    pylint

--- personalwebsite/models.py
+++ personalwebsite/models.py
@@ -1,13 +1,15 @@
 from personalwebsite import db
-from flask import current_app
+
 
 class Myself(db.Model):
     __tablename__ = "me"
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String, nullable=False, default="Kunhee")
     last_name = db.Column(db.String, nullable=False, default="Ha")
-    about = db.Column(db.String, nullable=False, default="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.")
-    email = db.Column(db.String, nullable=False, default="kunheeha@gmail.com")
+    about = db.Column(db.String, nullable=False, default=(
+        "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint"
+        " cillum sint consectetur cupidatat."
+        ))
     profile_photo = db.Column(db.String, default="default_profile_photo.jpg")
     cv_file = db.Column(db.String)
 
