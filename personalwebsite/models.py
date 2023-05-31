e67bc644b9fdf38d4c93d4707e90ac5c5a8698f0
Kunhee Ha <kunheeha@gmail.com>
2023-05-31 17:21:09 +0100

    add new table for each contact detail

--- personalwebsite/models.py
+++ personalwebsite/models.py
@@ -12,6 +12,16 @@ class Myself(db.Model):
         ))
     profile_photo = db.Column(db.String, default="default_profile_photo.jpg")
     cv_file = db.Column(db.String)
+    contacts = db.relationship("Contact", backref="me")
+
+
+class Contact(db.Model):
+    __tablename__ = "contacts"
+    id = db.Column(db.Integer, primary_key=True)
+    contact_type = db.Column(db.String, nullable=False)
+    link = db.Column(db.String, nullable=False)
+    fa_icon = db.Column(db.String(50), nullable=False, default="fa-link")
+    me_id = db.Column(db.Integer, db.ForeignKey("me.id"))
 
 
 class Skill(db.Model):
