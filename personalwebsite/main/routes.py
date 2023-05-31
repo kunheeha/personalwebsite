c5be14af699fcc1c335e8d095156b245f302952f
Kunhee Ha <kunheeha@gmail.com>
2023-05-31 17:26:19 +0100

    get contacts by me id and access from index.html

--- personalwebsite/main/routes.py
+++ personalwebsite/main/routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, current_app
 from personalwebsite import db
-from personalwebsite.models import Myself, Skill
+from personalwebsite.models import Myself, Contact
 
 
 main = Blueprint('main', __name__)
@@ -10,5 +10,7 @@ main = Blueprint('main', __name__)
 def index():
     myid = int(current_app.config['MYSELF_ID'])
     me = db.get_or_404(Myself, myid)
+    contacts = db.session.execute(
+            db.select(Contact).filter_by(me_id=myid).scalars())
 
-    return render_template('index.html', me=me)
+    return render_template('index.html', me=me, contacts=contacts)
